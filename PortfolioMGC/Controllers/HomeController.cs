05b8cadfe0a445f582ee2c2be0cd4f600fd1942e
2ndyrGroupC <2ndyrGroupC@pn.local>
2020-07-28 18:25:46 +0800

    ASP Portfolio Project

---
+++ PortfolioMGC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioMGC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Resume()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+    }
+}
